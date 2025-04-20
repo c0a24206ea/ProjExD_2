f81eda50d8a8f745b3a367d50cee8820c5b06a5a
C0A24206 <c0a24206ea@edu.teu.ac.jp>
2025-04-20 15:38:01 +0900

    いろいろ

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,21 +8,21 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    pg.display.set_caption("逃げろ！こうかとん") #ウィンドウタイトル
+    screen = pg.display.set_mode((WIDTH, HEIGHT)) #スクリーン
+    bg_img = pg.image.load("fig/pg_bg.jpg")   #背景画像のロード
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9) #0.9倍画像
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 300, 200
+    kk_rct.center = 300, 200 #Rect取得、画像の初期位置を300, 200 に設定
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0]) #背景画像の表示
 
-        key_lst = pg.key.get_pressed()
+        key_lst = pg.key.get_pressed() #押キー取得
         sum_mv = [0, 0]
         if key_lst[pg.K_UP]:
             sum_mv[1] -= 5
@@ -34,7 +34,7 @@ def main():
             sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
-        pg.display.update()
+        pg.display.update() #画面の更新
         tmr += 1
         clock.tick(50)
 
