39cbdee8b238cf3eed374c387afa6b203902a211
C0A24206 <c0a24206ea@edu.teu.ac.jp>
2025-04-22 16:54:15 +0900

    演習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,11 +2,14 @@ import os
 import random
 import sys
 import pygame as pg
-
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP : (0,-5) , pg.K_DOWN : (0,+5), pg.K_LEFT : (-5,0), pg.K_RIGHT : (5,0) }  #辞書生成　練習1
+
+    #bb_img = bb_imgs[min(tmr//500, 9)]tmrに応じて増加
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
@@ -22,9 +25,29 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def gameover(screen: pg.Surface) -> None:
+    """
+    gameoverになった際の判定
+    引数：Screen
+    画面を暗くする、こうかとん、GameOverの表示
+    5秒停止
+    """
+    blackout = pg.Surface((WIDTH, HEIGHT))
+    fonto  = pg.font.Font(None, 80)
+    text = fonto.render("Game Over", True, (255,255,255))
+    cry_img = pg.image.load("../Lecture02/fig/8.png")
 
+    pg.draw.rect(blackout, (0,0,0), (0, 0, WIDTH, HEIGHT))
+    blackout.set_alpha(200)
 
+    screen.blit(blackout, (0,0))
+    screen.blit(text, [400, 300])
+    screen.blit(cry_img, (350, 300))
+    screen.blit(cry_img, (730, 300))
 
+    pg.display.update()
+    time.sleep(5)
+    
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -33,24 +56,34 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    tmr = 0
+    bb_accs = [a for a in range(1,11)]  #加速度のリスト 10段階
+    vx, vy = +5, -5
 
     bb_img = pg.Surface((20, 20))  #円の生成　練習2
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+
+    #for r in range(1, 11):
+            #bb_img = pg.Surface((20*r, 20*r))
+    #pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
     bb_img.set_colorkey((0, 0, 0))
-    vx, vy = +5, -5
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     clock = pg.time.Clock()
-
-    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
 
-        if kk_rct.colliderect(bb_rct):  #こうかとんRectと爆弾Rectの衝突の判定
+
+        #if kk_rct.colliderect(bb_rct):  #こうかとんRectと爆弾Rectの衝突の判定
+            #print("GAMEOVER")
+            #return
+        
+        if kk_rct.colliderect(bb_rct):
             print("GAMEOVER")
+            gameover(screen)
             return
         
         key_lst = pg.key.get_pressed()
@@ -65,7 +98,10 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+
+        avx = vx*bb_accs[min(tmr//500, 9)]  #ばくだんの速度(x方向)
+        avy = vy*bb_accs[min(tmr//500, 9)]  #ばくだんの速度(y方向)
+        bb_rct.move_ip(avx, avy)
 
         yoko, tate = check_bound(bb_rct)
         if not yoko: #左右のはみ出し
